1632737617fc620af5d6ecf09b3cb1435da4e246
Bosnan <brayancordova9900@gmail.com>
2023-11-19 13:46:30 -0600

    adding kepler_data and rebuilding planets model

--- server-backend/src/models/planets.model.js
+++ server-backend/src/models/planets.model.js
@@ -1,3 +1,48 @@
-const planets = [];
+require('colors');
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+// filter out only planets that are habitable
+function isHabitablePlanet(planet) {
+    return (
+        planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 &&
+        planet['koi_insol'] < 1.11 &&
+        planet['koi_prad'] < 1.6
+    );
+}
+
+// read data
+fs.createReadStream('kepler_data.csv')
+    .pipe(
+        parse({
+            comment: '#',
+            columns: true,
+        })
+    )
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        console.log(
+            habitablePlanets.map((planet) => {
+                return planet['kepler_name'];
+            })
+        );
+        console.log(
+            `${
+                habitablePlanets.length.toString().green
+            } habitable planets found!!`
+        );
+    });
+
+module.exports = {
+    planets: habitablePlanets,
+};
